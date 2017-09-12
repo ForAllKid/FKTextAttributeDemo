7409aa883aa354918ebbcc6549442abec0d16b84
ForKid <910784384@qq.com>
2017-09-12 09:26:24 +0800

    2.0版本

---
+++ Desktop/GYLib/GYLib/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GYLib
+//
+//  Created by CDBottleApp on 2016/11/1.
+//  Copyright © 2016年 CDBottleApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Desktop/GYLib/Pods/Target Support Files/Pods-GYLib/Pods-GYLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GYLib : NSObject
+@end
+@implementation PodsDummy_Pods_GYLib
+@end
